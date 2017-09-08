4fdcbe43a6fb409f638daccfdc2d1d520d87cd0e
vrisolo <vrisolo@iad2.it>
2017-09-08 11:26:22 +0200

    1st commit

---
+++ src/main/java/it/risolo/GetJobDone/GetJobDoneApplication.java
@@ -0,0 +1,12 @@
+package it.risolo.GetJobDone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetJobDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetJobDoneApplication.class, args);
+	}
+}
---
+++ src/test/java/it/risolo/GetJobDone/GetJobDoneApplicationTests.java
@@ -0,0 +1,16 @@
+package it.risolo.GetJobDone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GetJobDoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
